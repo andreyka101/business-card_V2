521f6bc75f88dc721694a2bfbb693cfbe833a2db
andreyka101 <andrejkvasnin236@gmail.com>
2024-01-04 13:25:05 +0300

    grrgrgrg

--- src/main.ts
+++ src/main.ts
@@ -2,24 +2,51 @@ import './style.scss'
 
 
 const radioPack = document.querySelector("#settings #radioBox #pack") as HTMLDivElement
-const radioPackALL = document.querySelectorAll("#settings #radioBox #pack label") as any
+const lessonPayment = document.querySelectorAll("#rates #cards .priceList .price h4") as Record<number, HTMLElement>
 const radioLesson = document.querySelector("#settings #radioBox #lesson") as HTMLDivElement
 
-radioPack?.addEventListener('click',(e)=>{
+
+radioPack?.addEventListener('click', (e) => {
     let target = e.target as HTMLElement
     if (target.className != "inputPack") return
-    // console.log(target)
-    // radioPack.style.color = "black"
-    // console.log("============")
-    let tergN = target.parentNode?.childNodes[1] as HTMLInputElement
-    // console.log(tergN.dataset.pack)
-    for(let i of radioPackALL){
-        i.style.color = "black"
-        console.log(i)
+
+    let previousElement = target.previousSibling as any
+    previousElement = previousElement.previousSibling as HTMLInputElement
+    console.log(previousElement.dataset.pack)
+
+    console.log(lessonPayment);
+    //LINK - спросить
+    // for(let i in lessonPayment){
+    //     console.log(i);
+    // }
+    //LINK - спросить
+    for (let i of lessonPayment) {
+        let lessonPaymentTEXT = i.textContent.trim()
+        console.log(lessonPaymentTEXT);
+        let price = ""
+        let time = ""
+        let border = true
+        for(let index in lessonPaymentTEXT){
+            if (lessonPaymentTEXT[index] == " ") border = false
+            if (border){
+                price += lessonPaymentTEXT[index]
+            }
+            // console.log(index);
+            
+        }
+        console.log(price);
+        
     }
-    target.style.color = "red"
 })
-radioLesson?.addEventListener('click',()=>{
-    alert("radioLesson")
+
+radioLesson?.addEventListener('click', (e) => {
+    let target = e.target as HTMLElement
+    if (target.className != "inputLesson") return
+
+    let previousElement = target.previousSibling as any
+    previousElement = previousElement.previousSibling as HTMLInputElement
+    console.log(previousElement.id)
+
+
 })
 
