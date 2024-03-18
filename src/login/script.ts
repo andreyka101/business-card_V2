27229ceb91b7843d8c28b50f351b6db1e3666687
andreyka101 <andrejkvasnin236@gmail.com>
2024-03-18 19:17:10 +0300

    цвцв

--- src/login/script.ts
+++ src/login/script.ts
@@ -1,1 +1,30 @@
 import './style.scss'
+
+
+const createUser = document.querySelector('#createUser')
+const name = document.querySelector('#name') as HTMLInputElement
+const surname = document.querySelector('#surname') as HTMLInputElement
+const email = document.querySelector('#email') as HTMLInputElement
+const password = document.querySelector('#password') as HTMLInputElement
+ 
+createUser?.addEventListener('click',async()=>{
+    name.value = name.value.trim()
+    surname.value = surname.value.trim()
+    email.value = email.value.trim()
+
+
+
+    let obj = {
+        'email':email.value,
+        'name':name.value,
+        'surname':surname.value,
+        'password':password.value,
+    }
+    let ping = await fetch("http://localhost:3000/registration", {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json;charset=utf-8'
+        },
+        body: JSON.stringify(obj)
+    })
+})
