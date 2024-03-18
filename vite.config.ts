698be8b15b4bd9c48a854d4459503d7e7c50fa4f
andreyka101 <andrejkvasnin236@gmail.com>
2024-03-18 11:55:02 +0300

    wddwwd

--- vite.config.ts
+++ vite.config.ts
@@ -1,3 +1,15 @@
+import {resolve} from 'path'
+
 export default {
-  base: '/business-card_V2/'
+  base: '/business-card_V2/',
+  build: {
+    rollupOptions: {
+      input: {
+        // @ts-ignore
+        main: resolve(__dirname, 'index.html'),
+        // @ts-ignore
+        movie: resolve(__dirname, '/src/entrance/entrance.html'),
+      }
+    }
+  }
 }
