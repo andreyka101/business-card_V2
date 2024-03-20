de6da6b1899606a2c5db03bc31d8b76500972210
andreyka101 <andrejkvasnin236@gmail.com>
2024-03-20 15:31:50 +0300

    effefef

--- src/login/script.ts
+++ src/login/script.ts
@@ -7,8 +7,8 @@ const name = document.querySelector('#name') as HTMLInputElement
 const surname = document.querySelector('#surname') as HTMLInputElement
 const email = document.querySelector('#email') as HTMLInputElement
 const password = document.querySelector('#password') as HTMLInputElement
- 
-createUser?.addEventListener('click',async()=>{
+
+createUser?.addEventListener('click', async () => {
     name.value = name.value.trim()
     surname.value = surname.value.trim()
     email.value = email.value.trim()
@@ -16,10 +16,10 @@ createUser?.addEventListener('click',async()=>{
 
 
     let obj = {
-        'email':email.value,
-        'name':name.value,
-        'surname':surname.value,
-        'password':password.value,
+        'email': email.value,
+        'name': name.value,
+        'surname': surname.value,
+        'password': password.value,
     }
     let ping = await fetch("http://localhost:3000/registration", {
         method: 'POST',
@@ -28,29 +28,30 @@ createUser?.addEventListener('click',async()=>{
         },
         body: JSON.stringify(obj)
     })
-    
+
 })
- 
-entrance?.addEventListener('click',async()=>{
+
+entrance?.addEventListener('click', async () => {
     name.value = name.value.trim()
     surname.value = surname.value.trim()
-    
-    
-
-
-    let obj = {
-        'name':name.value,
-        'surname':surname.value,
-        'password':password.value,
+    // window.location.href = "../user/client.html"
+    try {
+        let obj = {
+            'name': name.value,
+            'surname': surname.value,
+            'password': password.value,
+        }
+        let ping = await fetch("http://localhost:3000/entrance", {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json;charset=utf-8'
+            },
+            body: JSON.stringify(obj)
+        }) as any
+        ping = await ping.json()
+        if(ping.name == name.value) window.location.href = "../user/client.html"
     }
-    let ping = await fetch("http://localhost:3000/entrance", {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json;charset=utf-8'
-        },
-        body: JSON.stringify(obj)
-    })
-    ping = await ping.json()
-
-    console.log(ping);
+    catch{
+        
+    }
 })
